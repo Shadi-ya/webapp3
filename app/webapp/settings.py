96da30d6b1dc67d89d5cba8c0e886ae784c3b330
wasimma <wasim.mohammed@genproresearch.com>
2023-02-20 11:19:17 +0530

    django format

--- app/webapp/settings.py
+++ app/webapp/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- app/webhome/views.py
+++ app/webhome/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'webhome/index.html')
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'webhome/login.html')
 
 def doctorlogin(request):
-    return render(request, 'doctorlogin.html')
+    return render(request, 'webhome/doctorlogin.html')
 
 def profile(request):
-    return render(request, 'profile.html')
+    return render(request, 'webhome/profile.html')
